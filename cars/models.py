9b0d6c8051cf112093c93d4e36df2d07df01e8fc
Andrey Baranyuk <andrew-baranuk@yandex.ru>
2021-10-01 13:37:43 +0300

    Added models

---
+++ cars/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Car(models.Model):
+    vin = models.CharField(max_length=64)
+    color = models.CharField(max_length=64)
+    brand = models.CharField(max_length=64)
+    CAR_TYPES = (
+        (1, 'Седан'),
+        (2, 'Хэтчбэк'),
+        (3, 'Универсал'),
+        (4, 'Купе')
+    )
+    car_type = models.IntegerField(choices=CAR_TYPES)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
