c63ab49141aa4b2a7d04b93dec4d601973a9f965
Andrey Baranyuk <andrew-baranuk@yandex.ru>
2021-10-09 21:27:50 +0300

    Read, update, delete API

--- cars/urls.py
+++ cars/urls.py
@@ -4,5 +4,7 @@ from .views import *
 app_name = 'cars'
 urlpatterns = [
     path('car/create/', CarCreateView.as_view()),
-    path('all/', CarsListView.as_view())
+    path('all/', CarsListView.as_view()),
+    path('car/detail/<int:pk>/', CarDetailView.as_view())
+
 ]
